142ad3a1fa8020714c481bdbaf3961a4ad098b18
Tsenda-Djessy-Flavien <djesstsenda@gmail.com>
2023-06-11 16:01:04 +0100

    Explorer les modificateurs d'événement

--- app.js
+++ app.js
@@ -4,20 +4,34 @@ const app = createApp({
   data() {
     return {
       counter: 0,
-      name: '',
-    }
+      name: "",
+      confirmName: "",
+    };
   },
-  methods:  {
+  methods: {
+    // //-------- 1 methods
+    // submitForm(event) {
+    //   event.preventDefault(); // ne pas recharger la plage, lors qu'on sur le button qui se trouve dans le formulaire
+    //   alert('submitted...');
+    // },
+    submitInput() {
+      this.confirmName = this.name;
+    },
+    // //-------- 2 methods
+    submitForm() {
+      //  event.preventDefault(); // ne pas recharger la plage, lors qu'on sur le button qui se trouve dans le formulaire
+      alert("submitted");
+    },
     setName(event, lastName) {
-      this.name = event.target.value + ' ' + lastName;
+      this.name = event.target.value + " " + lastName;
     },
     increment(num) {
       this.counter = this.counter + num;
     },
     decrement(num) {
-      this.counter = this.counter - num;;
+      this.counter = this.counter - num;
     },
   },
 });
 
-app.mount('#events');
+app.mount("#events");
